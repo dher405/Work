0a664e2ce26c15a06663cbaad40552db7cbf4d7b
dher405 <david.herron74@gmail.com>
2025-03-01 22:39:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 app = FastAPI()
 
 # Load PostgreSQL connection URL from environment variable (recommended for security)
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:GIE92mhtkxdJw1SD@kyjqfeyfsnvsyvugsncy.supabase.co:5432/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres.kyjqfeyfsnvsyvugsncy:GIE92mhtkxdJw1SD@aws-0-us-west-1.pooler.supabase.com:6543/postgres")
 
 # Set up SQLAlchemy engine & session
 engine = create_async_engine(DATABASE_URL, echo=True)
