b06c61d6ba51941eefea85fac441cc6de7e31346
dher405 <david.herron74@gmail.com>
2025-03-03 18:10:16 -0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,6 @@
-
+#!/usr/bin/env bash
+echo "Installing Chrome..."
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
