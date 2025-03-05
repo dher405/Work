ea1d1039491e413898e1ab87613e6be839415186
dher405 <david.herron74@gmail.com>
2025-03-05 14:30:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
